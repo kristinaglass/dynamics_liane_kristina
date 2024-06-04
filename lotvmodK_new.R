6c636b15663e74ff766a4be0a67064bb605feb35
liane@ucsb.edu <liane@ucsb.edu>
2024-06-03 20:34:33 -0700

    added hunting to function

--- lotvmodK_new.R
+++ lotvmodK_new.R
@@ -13,7 +13,7 @@
 #' @examples
 #' lotvmodH(t=1, pop=list(prey=1, pred=2), pars=list(rprey=0.5, alpha=0.3, eff=0.2, pmort=0.2, hunt_prey=0.1, hunt_pred=0.05))
 #'
-#' pars = c(rprey=0.5, alpha=0.3, eff=0.2, pmort=0.2)
+#' pars = c(rprey=0.5, alpha=0.3, eff=0.2, pmort=0.2, hunt_prey=0.1, hunt_pred=0.05)
 #' currpop  = c(prey = 1, pred=1)
 #  days = seq(from=1,to=20)
 #' res = ode(func=lotvmod, y=currpop, times=days, parms=pars)
@@ -26,8 +26,8 @@
 
 lotvmodK = function(t, pop, pars) {
 with(as.list(c(pars,pop)), {
-dprey = rprey*(1-prey/K)*prey -  alpha*prey*pred
-dpred = eff*alpha*prey*pred - pmort*pred
+dprey = rprey*(1-prey/K)*prey -  alpha*prey*pred - hunt_prey*prey
+dpred = eff*alpha*prey*pred - pmort*pred- hunt_pred*pred
 return(list(c(dprey,dpred)))})
 }
 
